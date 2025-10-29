9772f96be35e3a8f7afc29b927f0c1f2ab277aea
Cinniven <Mu8988@gmail.com>
2025-10-29 09:53:29 +0100

    Test

---
+++ Assets/_Scripts/ObjectData.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using System;
+
+[Serializable]
+public class ObjectData
+{
+    public string name;
+    public Vector3 position;
+    public Quaternion rotation;
+    public Vector3 size;
+}
